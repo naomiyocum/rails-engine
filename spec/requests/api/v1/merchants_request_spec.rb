ea10c7dc5185ea84578e22c8b05fa0da4e58b97b
Naomi Yocum <naomiyocum24@gmail.com>
2022-12-05 15:07:07 -0700

    Complete Get One Merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -5,10 +5,9 @@ describe 'Merchants API' do
     create_list(:merchant, 5)
 
     get api_v1_merchants_path
+    merchants = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-
-    merchants = JSON.parse(response.body, symbolize_names: true)
    
     expect(merchants).to be_a(Hash)
     expect(merchants[:data].count).to eq(5)
@@ -27,5 +26,26 @@ describe 'Merchants API' do
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a(String)
     end
+  end
+
+  it 'can get one merchant by id' do
+    id = create(:merchant).id
+
+    get api_v1_merchant_path(id)
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id]).to be_a(String)
+
+    expect(merchant[:data]).to have_key(:type)
+    expect(merchant[:data][:type]).to be_a(String)
+
+    expect(merchant[:data]).to have_key(:attributes)
+    expect(merchant[:data][:attributes]).to be_a(Hash)
+
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
   end
 end

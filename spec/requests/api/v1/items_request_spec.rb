afae24f6a925d6fa77887ea31f3873955c51a004
Naomi Yocum <naomiyocum24@gmail.com>
2022-12-05 16:07:00 -0700

    Complete Create Item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -66,4 +66,24 @@ describe 'Items API' do
     expect(attributes).to have_key(:merchant_id)
     expect(attributes[:merchant_id]).to be_an(Integer)
   end
+
+  it 'can create a new item' do
+    id = create(:merchant).id
+    item_params = ({
+      name: 'Mesh Shorts',
+      description: 'Super comfy',
+      unit_price: 19.99,
+      merchant_id: id
+    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post '/api/v1/items', headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+   
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
 end
